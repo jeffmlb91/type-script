f456515847faabbe0c10bdaa5a1a228540897300
jeffmlb91 <jeffmlb91@gmail.com>
2023-06-07 11:55:13 -0400

    refactored code: add if statement to check typeog num1 and num2

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  return num1 + num2;
+ // Telling javascript that if num1 and num 2 type is not a number return num1 + num 2 
+  if( typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2
+ } else {
+    return +num1 + +num2;
+ }  
+ 
 }
-document.addEventListener("DOMContentLoaded", function () {
-  button.addEventListener("click", function () {
+
+button.addEventListener("click", function () {
     console.log(add(input1.value, input2.value));
   });
-});
